c789c546c81e3e2d1c2cfea5e40f59f232d27ec2
Ablesax <shalomableone@gmail.com>
2023-09-11 12:47:02 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - A c program that print to standard error
+ * Return 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
